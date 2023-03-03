b5aac2e98ad4849ee3aa17b7bf660cde927963e6
unknown <staracenapuga@gmail.com>
2023-03-03 10:38:36 -0600

    Finished epsilon closure and move

--- src/main.py
+++ src/main.py
@@ -5,13 +5,15 @@ Santiago Taracena Puga (20017)
 """
 
 # Módulos necesarios para la construcción del NFA.
-from utils.postfix import regex_infix_to_postfix, check_concatenations
+from utils.postfix import regex_infix_to_postfix
 from utils.thompson import thompson_construction
+from utils.subsets import subset_construction
 from utils.show_fa import show_nfa
 
 # Transformación de la expresión regular a postfix y construcción del NFA.
 regex = input("Please, input a regex: ")
 postfix = regex_infix_to_postfix(regex)
 nfa = thompson_construction(postfix)
+subset_construction(nfa)
 show_nfa(nfa)
 print(nfa)
---
+++ src/utils/classes/dfa.py
@@ -0,0 +1,27 @@
+"""
+Universidad del Valle de Guatemala
+(CC3071) Diseño de Lenguajes de Programación
+Santiago Taracena Puga (20017)
+"""
+
+# Definición de la clase DFA.
+class DFA(object):
+
+    # Método constructor de NFA que recibe estados, alfabeto, estado inicial, estado de aceptación y mapeo de transiciones.
+    def __init__(self, states, alphabet, initial_state, acceptance_states, mapping):
+        self.states = states
+        self.alphabet = alphabet
+        self.initial_state = initial_state
+        self.acceptance_states = acceptance_states
+        self.mapping = mapping
+
+    # Representación en string del NFA.
+    def __repr__(self):
+        string_representation = "NFA(\n"
+        string_representation += f"\tstates={self.states},\n"
+        string_representation += f"\talphabet={self.alphabet},\n"
+        string_representation += f"\tinitial_state={self.initial_state},\n"
+        string_representation += f"\tacceptance_state={self.acceptance_states},\n"
+        string_representation += f"\tmapping={self.mapping}\n"
+        string_representation += ")"
+        return string_representation
