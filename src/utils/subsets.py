160f2dd2485470129cc2083c6c356912d335b2b8
unknown <staracenapuga@gmail.com>
2023-03-10 22:50:03 -0600

    Finally finished epsilon-closure and move

--- src/utils/subsets.py
+++ src/utils/subsets.py
@@ -11,64 +11,54 @@ from utils.classes.dfa import DFA
 # Definición de la función ε-closure.
 def ε_closure(element, mapping):
 
-    # Si el elemento es un conjunto, se calcula ε-closure de todos sus elementos.
-    if (type(element) == set):
+    # Stack para almacenar los estados del conjunto.
+    state_stack = Stack()
 
-        # Stack para almacenar los estados del conjunto.
-        state_stack = Stack()
+    # Push de todos los estados del conjunto al stack.
+    for state in element:
+        state_stack.push(state)
 
-        # Push de todos los estados del conjunto al stack.
-        for state in element:
-            state_stack.push(state)
+    # Instancia inicial del resultado.
+    result = set()
 
-        # Instancia inicial del resultado.
-        result = set()
+    for state in element:
+        result.add(state)
 
-        # Mientras el stack no esté vacío, se obtiene el estado en el tope y se calcula su ε-closure.
-        while (not state_stack.is_empty()):
-            state = state_stack.pop()
-            if ("ε" in mapping[state]):
-                result |= mapping[state]["ε"]
+    # Mientras el stack no esté vacío, se obtiene el estado en el tope y se calcula su ε-closure.
+    while (not state_stack.is_empty()):
+        state = state_stack.pop()
+        if ("ε" in mapping[state]):
+            new_element = mapping[state]["ε"]
+            for reachable_state in new_element:
+                state_stack.push(reachable_state)
+            result |= new_element
 
-        # Retorno del resultado.
-        return result
-
-    # Si el elemento es un estado, se calcula su ε-closure.
-    else:
-
-        # Retorno del ε-closure del resultado.
-        return mapping[element]["ε"]
+    # Retorno del resultado.
+    return result
 
 # Definición de movimiento de estados con un símbolo.
 def move(element, symbol, mapping):
 
-    # Si el elemento es un conjunto, se calcula move de todos sus elementos.
-    if (type(element) == set):
+    # Stack para almacenar los estados del conjunto.
+    state_stack = Stack()
 
-        # Stack para almacenar los estados del conjunto.
-        state_stack = Stack()
+    # Push de todos los estados del conjunto al stack.
+    for state in element:
+        state_stack.push(state)
 
-        # Push de todos los estados del conjunto al stack.
-        for state in element:
-            state_stack.push(state)
+    # Instancia inicial del resultado.
+    result = set()
 
-        # Instancia inicial del resultado.
-        result = set()
+    # Mientras el stack no esté vacío, se obtiene el estado en el tope y se calcula su move.
+    while (not state_stack.is_empty()):
+        state = state_stack.pop()
+        if (symbol in mapping[state]):
+            result |= mapping[state][symbol]
 
-        # Mientras el stack no esté vacío, se obtiene el estado en el tope y se calcula su move.
-        while (not state_stack.is_empty()):
-            state = state_stack.pop()
-            if (symbol in mapping[state]):
-                result |= mapping[state][symbol]
-
-        # Retorno del resultado.
-        return result
-
-    # Si el elemento es un estado, se calcula su move.
-    else:
-
-        # Retorno del move del resultado.
-        return mapping[element][symbol]
+    # Retorno del resultado.
+    return result
 
 def subset_construction(nfa):
+    print(ε_closure({ nfa.initial_state, 1, 3 }, nfa.mapping))
+    print(move({ 0, 2 }, "a", nfa.mapping))
     pass
